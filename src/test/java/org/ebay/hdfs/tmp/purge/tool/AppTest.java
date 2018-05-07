55bf3e445d405d64fbf490f3b8cda2c4cfb3d228
lzhang32 <lzhang32@ebay.com>
2018-05-07 13:37:23 +0800

    first commit

---
+++ src/test/java/org/ebay/hdfs/tmp/purge/tool/AppTest.java
@@ -0,0 +1,20 @@
+package org.ebay.hdfs.tmp.purge.tool;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
